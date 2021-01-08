9dc7fcd0f9153c0f78e7227d93f1e0520aad93e7
mam1 <mam1@cornell.edu>
2021-01-08 15:23:14 -0500

    starting on linked lists and stacks

---
+++ include/typedefs.h
@@ -0,0 +1,17 @@
+#ifndef _TYPEDEFS_H_
+#define _TYPEDEFS_H_
+
+
+
+typedef struct {
+	head
+	tail
+} _list;
+
+typedef struct {
+	head
+	tail
+} _stack;
+
+
+#endif
---
+++ tests/stack/stacktest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include </home/mam1/Git/MQTT-Scheduler/include/list.h>
+#include </home/mam1/Git/MQTT-Scheduler/include/stack.h>
+#include </home/mam1/Git/MQTT-Scheduler/include/typedefs.h>
+
+
+main() {
+
+   printf("testing stack functions\n");
+
+
+
+   return 0;
+
+}
