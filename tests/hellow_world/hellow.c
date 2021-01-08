62f8ee93ce4056f3c7c80157ba17eb8d76392d42
mam1 <mam1@cornell.edu>
2021-01-08 15:22:57 -0500

    starting on linked lists and stacks

--- tests/hellow_world/hellow.c
+++ tests/hellow_world/hellow.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-// #include </home/mam1/Git/MQTT-Scheduler/include/ttlib.h>
+#include </home/mam1/Git/MQTT-Scheduler/include/ttlib.h>
 
 
-void ttlib(void);
 main() {
 
    printf("Hellow world!!!!!:\n");
